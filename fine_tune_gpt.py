c15d8135eec76fccc0d89a728a005f67546bd548
Aeteux <45810077+Aeteux@users.noreply.github.com>
2023-05-06 16:11:40 -0700

    add the load_dataset function

--- fine_tune_gpt.py
+++ fine_tune_gpt.py
@@ -3,6 +3,13 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config
 from transformers import TextDataset, DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
 
+def load_dataset(train_file, test_file, tokenizer):
+    # Load the training and testing datasets as TextDataset objects
+    # using the specified tokenizer and block_size
+    train_dataset = TextDataset(tokenizer=tokenizer, file_path=train_file, block_size=128)
+    test_dataset = TextDataset(tokenizer=tokenizer, file_path=test_file, block_size=128)
+    return train_dataset, test_dataset
+
 def main():
     pass
 
