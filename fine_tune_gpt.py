0b06a578ca7faaa4787a8c6b19a34238f3e477c9
Aeteux <45810077+Aeteux@users.noreply.github.com>
2023-05-06 16:05:22 -0700

    import required libraries and main function

---
+++ fine_tune_gpt.py
@@ -0,0 +1,10 @@
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config
+from transformers import TextDataset, DataCollatorForLanguageModeling
+from transformers import Trainer, TrainingArguments
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
