51344961021b2465495faa29642916291fd23a44
Aeteux <45810077+Aeteux@users.noreply.github.com>
2023-05-06 16:36:38 -0700

    set up data collator and training arguments

--- fine_tune_gpt.py
+++ fine_tune_gpt.py
@@ -26,5 +26,24 @@ def main():
     # Load datasets
     train_dataset, test_dataset = load_dataset(train_file, test_file, tokenizer)
 
+    # Set data collator
+    # The DataCollatorForLanguageModeling handles batching and token masking
+    # Set mlm to False since GPT-2 is an autoregressive model
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+    # Set training arguments
+    # Configure settings such as output directory, batch size, number of epochs, etc.
+    training_args = TrainingArguments(
+        output_dir='output',
+        overwrite_output_dir=True,
+        num_train_epochs=3,
+        per_device_train_batch_size=4,
+        per_device_eval_batch_size=4,
+        eval_steps=400,
+        save_steps=800,
+        warmup_steps=200,
+        prediction_loss_only=True,
+    )
+
 if __name__ == '__main__':
     main()
